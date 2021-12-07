e6288ae087cc7a2327a1613d775daedf5d2638a4
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-12-07 15:35:51 +0000

    completed all seven within time

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 //write your code here
+const animals =['cheetah','dog', 'cat', 'dodobird', 'bear', 'dolphin'];
+
+const pluralize = array => {
+    return array.map(element => `${element}s`);
+};
+console.log(pluralize(animals));
+
+const uppercase = array => {
+    return array.map(element => element[0].toUpperCase() + element.slice(1));
+};
+console.log(uppercase(animals));
+
+const longWords = array => {
+    return array.filter(string => string.length > 3);
+};
+console.log(longWords(animals));
+
+const shortWords = array => {
+    return array.filter(string => string.length < 5);
+};
+console.log(shortWords(animals));
+
+const oddLength = array => {
+    return array.filter(element => (element.length % 2) !== 0);
+};
+console.log(oddLength(animals));
+
+const longToShort = array => {
+    return array.sort((animal, nextAnimal) => nextAnimal.length - animal.length);
+};
+console.log(longToShort(animals));
+
+const sum = numsArray => {
+    return numsArray.reduce((accum, currentValue) => accum + currentValue);
+};
+console.log(sum([22, 15, 1114, 416, 37, 4]));
+
